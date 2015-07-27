e5d72ec2bb002ab75293b66d6c23739f7ab7d941
SimonRichardson <stickupkid@gmail.com>
2015-07-27 09:08:30 +0100

    Hooking up to the db

--- src/Model.hs
+++ src/Model.hs
@@ -1,19 +1,25 @@
-{-# LANGUAGE EmptyDataDecls        #-}
-{-# LANGUAGE FlexibleContexts      #-}
-{-# LANGUAGE FlexibleInstances     #-}
-{-# LANGUAGE GADTs                 #-}
-{-# LANGUAGE OverloadedStrings     #-}
-{-# LANGUAGE QuasiQuotes           #-}
-{-# LANGUAGE TemplateHaskell       #-}
-{-# LANGUAGE TypeFamilies          #-}
-{-# LANGUAGE TypeSynonymInstances  #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE EmptyDataDecls             #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE TypeSynonymInstances       #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DeriveGeneric              #-}
 
 module Model where
 
+import Data.Aeson (ToJSON)
+
 import Database.Persist.MongoDB hiding (master)
 import Database.Persist.TH
+
+import GHC.Generics
+
 import Language.Haskell.TH.Syntax
 
 let mongoSettings = (mkPersistSettings (ConT ''MongoContext)) { mpsGeneric = False }
@@ -21,5 +27,7 @@ let mongoSettings = (mkPersistSettings (ConT ''MongoContext)) { mpsGeneric = Fal
 Event
     name String
     description String
-    deriving Show
+    deriving Show Generic
 |]
+
+instance ToJSON Event
--- src/Server.hs
+++ src/Server.hs
@@ -1,10 +1,13 @@
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE TypeFamilies               #-}
+
 module Main where
 
 import Web.Scotty as W
 
 import Model as M
 
-import Network.HTTP.Types.Status (notFound404)
 import Network.HTTP.Types.Method (StdMethod(OPTIONS))
 import Network.Wai (Middleware)
 import Network.Wai.Middleware.AddHeaders
@@ -13,24 +16,27 @@ import           DatabaseConn
 import           Database.Persist hiding (get)
 import qualified Database.Persist as DB
 
-import Data.Maybe (fromMaybe, isJust)
-import Data.List (elemIndex, mapAccumR, transpose)
-import Data.Text.Lazy (fromStrict, toStrict)
+import Data.Aeson
+import Data.Maybe (fromMaybe)
+
+import Debug.Trace
 
 import System.Environment (getEnvironment)
-import Control.Monad.Trans (liftIO)
+
+import Control.Monad.Reader
 
 main :: IO ()
 main = do
   conn <- getEnvDef "MONGOLAB_URI" devMongo >>= return . parseDatabaseUrl
   port <- fmap read $ getEnvDef "PORT" "8000"
 
-  let conf = mongoConfFrom conn
+  let conf = mongoConfFrom x
+
   pool <- createPoolConfig conf
   scotty port (app conf pool)
 
   where
-    devMongo = "mongodb://127.0.0.1:27017/events"
+    devMongo = "mongodb://127.0.0.1:27017/dice"
     getEnvDef e d = getEnvironment >>= return . fromMaybe d . lookup e
 
 allowCors :: Middleware
@@ -39,12 +45,27 @@ allowCors = addHeaders [
     ("Access-Control-Allow-Headers", "Accept, Content-Type")    
   ]
 
+contentType :: Middleware
+contentType = addHeaders [
+    ("Content-Type", "text/json")
+  ]
+
 app conf pool = do
   middleware allowCors
+  middleware contentType
   opt "/" $ "GET"
   get "/" $ do
-    W.text "FUCK"
+    events <- getEvents
+    let x = (trace "here") events
+    W.json $ toOutput <$> (if null x then [] else x)
 
   where
+    runDB action          = liftIO $ runPool conf action pool
+    getEvents             = runDB $ DB.selectList [] []
+    -- move to model.hs
+    toOutput (Entity _ s) = object [ "name"        .= eventName s
+                                   , "description" .= eventDescription s
+                                   ]
+    
     opt route opts  = addroute OPTIONS route $ do
       setHeader "Access-Control-Allow-Methods" opts
